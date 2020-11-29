1d2964fea1754551d822eaa8764652062170508d
ValNicolle <valentin.nicolle@yahoo.fr>
2020-11-29 17:25:36 +0100

    Search feature OK

--- bookstore/src/Home.js
+++ bookstore/src/Home.js
@@ -6,40 +6,34 @@ function Home () {
 
 	const [initialBooks, setInitialBooks] = useState([])
 	const [books, setBooks] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
+  //API call
 	useEffect(() => {
-        fetch("http://henri-potier.xebia.fr/books", {method: 'GET'})
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setBooks(result) 
-            setInitialBooks(result) 
-            setIsLoading(false)
-            console.log(result);
-          },
-          (error) => {
-            console.log(error)
-          }
-        )
-    }, [])
+      fetch("http://henri-potier.xebia.fr/books")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setBooks(result) 
+          setInitialBooks(result) 
+          setIsLoading(false)
+          console.log(result);
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, [])
     
-    
-    const handleSearch = (evt) =>{
-        //console.log(initialBooks[0].title.includes(evt.target.value));
-        let entry = evt.target.value.toLowerCase()
-        //let newData = initialBooks.filter(bk => bk.map(b => b.includes(entry)))
-        // let newData = initialBooks.map(bk => {
-        //     bk.reduce((acc, text) => acc + )
-        // })
-        // const newData = initialBooks.filter(livre => livre.reduce((acc, bookin)=> acc + bookin.title + bookin.synopsis[0]).includes(entry))
-        console.log(newData)
-    //     if(entry == ""){
-    //         newData = initialBooks
-    //     }
-    //    setBooks(newData)
-        
-    }
+  // Search function
+  const handleSearch = (evt) => {
+      let entry = evt.target.value.toLowerCase() // Input
+      let filteredData = initialBooks.filter((book) => { // Filter data inside Title an Synopsis
+        return book.title.toLowerCase().includes(entry) || book.synopsis.join(' ').toLowerCase().includes(entry)
+        })
+      setBooks(filteredData) //set filtered data
+      
+  }
 
 
 
@@ -50,16 +44,16 @@ function Home () {
 	else{
 		// When data is charged
 	return (
-            <div className="main">
-                <div className="search-ctn">
-                    <input onChange={handleSearch} type="text" />
-                </div>
-                <div className="books-ctn">
-                    {books.map((book, index) => (
-                        <BookItem key={index} book={book} />
-                    ))}
-                </div>
+        <div className="main">
+            <div className="search-ctn">
+                <input onChange={handleSearch} type="text" placeholder="Recherchez un terme"/>
             </div>
+            <div className="books-ctn">
+                {books.map((book, index) => (
+                    <BookItem key={index} book={book} />
+                ))}
+            </div>
+        </div>
 		);
 	}
 			
